c909caed3979f38ff46e7a468615df8510d9f653
Amoda Dissanayake <amodadissanayake@gmail.com>
2018-01-28 15:54:33 +0530

    add spring dependencies

--- src/main/java/com/restuarant/app/controller/OrderController.java
+++ src/main/java/com/restuarant/app/controller/OrderController.java
@@ -1,4 +1,14 @@
 package com.restuarant.app.controller;
 
 public class OrderController {
+
+    public String welcome() {//Welcome page, non-rest
+        return "Welcome to RestTemplate Example.";
+    }
+
+//    public Message message(@PathVariable String player) {//REST Endpoint.
+//
+//        Message msg = new Message(player, "Hello " + player);
+//        return msg;
+//    }
 }
